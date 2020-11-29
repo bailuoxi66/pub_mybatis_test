5acb38b34f212d2e42ae2155613ff091567ea918
liangzhengtao.lzt <liangzhengtao.lzt@lazada.com>
2020-11-29 11:14:33 +0800

    [feature] "引入log4j，并理解配置文件和测试"

--- Mybatis-Study/Mybatis-01/src/test/java/com/example/dao/UserDaoTest.java
+++ Mybatis-Study/Mybatis-01/src/test/java/com/example/dao/UserDaoTest.java
@@ -3,12 +3,14 @@ package com.example.dao;
 import com.example.pojo.User;
 import com.example.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.util.List;
 
 public class UserDaoTest {
 
+    static Logger logger = Logger.getLogger(UserDaoTest.class);
     @Test
     public void test(){
         //第一步获取sqlSession对象
@@ -87,5 +89,12 @@ public class UserDaoTest {
         sqlSession.commit();
         //关闭SqlSession
         sqlSession.close();
+    }
+
+    @Test
+    public void testLog4j(){
+        logger.info("info: 进入了testLog4j");
+        logger.debug("debug: 进入了log4j");
+        logger.error("error: 进入了testLog4j");
     }
 }
